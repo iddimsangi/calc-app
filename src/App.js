af35d6ff2a4e1cf356e8ffa60aa6c51451c531e3
iddimsangi <adammsangi@gmail.com>
2023-11-10 08:50:21 +0300

    fix config file error

---
+++ src/App.js
@@ -0,0 +1,40 @@
+function App() {
+  return (
+    <div>
+      <div className=" bg-black h-screen flex flex-col space-y-3 p-4 justify-center items-center">
+        <div className=" flex justify-between w-full bg-orange-500">
+          <h1 className="">Calc</h1>
+          <div className="flex w-32 items-center bg-green-500">
+            <h3>Theme</h3>
+            <input type="checkbox" />
+          </div>
+        </div>
+        <div className="flex justify-end bg-red-800">
+          <h1>455,981</h1>
+        </div>
+        <div className=" grid grid-cols-4 grid-rows-5 gap-2 bg-yellow-600">
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+          <a hrf="#">bt</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        mainBackground: "hsl(222, 26%, 31%)",
+        toggleBackground: "hsl(223, 31%, 20%)",
+        keypadBackground: "hsl(223, 31%, 20%)",
+        screenBackground: "hsl(224, 36%, 15%)",
+        keyBackground: "hsl(225, 21%, 49%)",
+        keyShadow: "hsl(224, 28%, 35%)",
+        redKeyBackground: "hsl(6, 63%, 50%)",
+        redKeyShadow: "hsl(6, 70%, 34%)",
+        lightGrayishOrangeKeyBackground: "hsl(30, 25%, 89%)",
+        grayishOrangeKeyShadow: "hsl(28, 16%, 65%)",
+        veryDarkGrayishBlueText: "hsl(221, 14%, 31%)",
+        whiteText: "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
