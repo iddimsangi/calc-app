2cfd1a7b1031ed5f3383a88c7d9a1df824724ac7
iddimsangi <adammsangi@gmail.com>
2023-11-15 15:43:20 +0300

    feat:styling the heading content

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 function App() {
   return (
     <div>
-      <div className=" bg-black h-screen flex flex-col space-y-3 p-4 justify-center items-center">
-        <div className=" flex justify-between w-full bg-orange-500">
-          <h1 className="">Calc</h1>
-          <div className="flex w-32 items-center bg-green-500">
-            <h3>Theme</h3>
-            <input type="checkbox" />
+      <div className="bg-mainBackground h-screen flex flex-col space-y-3 p-4 justify-center items-center">
+        <div className=" min-w-[600px]">
+          <div className="flex justify-between w-full bg-orange-500 py-3">
+            <h1 className=" text-whiteText font-bold text-2xl">Calc</h1>
+            <div className="flex w-32 items-center bg-green-500 justify-between">
+              <h3 className=" text-xs text-white uppercase">Theme</h3>
+              <input type="checkbox" />
+            </div>
           </div>
-        </div>
-        <div className="flex justify-end bg-red-800">
-          <h1>455,981</h1>
-        </div>
-        <div className=" grid grid-cols-4 grid-rows-5 gap-2 bg-yellow-600">
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
-          <a hrf="#">bt</a>
+          <div className="flex justify-end bg-red-800 py-5 text-3xl font-bold text-white px-2">
+            <h1>455,981</h1>
+          </div>
+          <div className=" grid grid-cols-4 grid-rows-5 gap-2 bg-yellow-600">
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+            <a hrf="#">bt</a>
+          </div>
         </div>
       </div>
     </div>
